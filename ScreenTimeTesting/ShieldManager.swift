e5e2c6b041abf4cd80ab6eb1c0483739e8d48760
Iftikhar Hussain <iftikharhussain2230@gmail.com>
2024-09-26 00:21:06 +0500

    Initial commit copy code

---
+++ ScreenTimeTesting/ShieldManager.swift
@@ -0,0 +1,8 @@
+//
+//  ShieldManager.swift
+//  ScreenTimeTesting
+//
+//  Created by mac on 26/09/2024.
+//
+
+import Foundation
---
+++ ScreenTimeTesting/ShieldView.swift
@@ -0,0 +1,18 @@
+//
+//  ShieldView.swift
+//  ScreenTimeTesting
+//
+//  Created by mac on 26/09/2024.
+//
+
+import SwiftUI
+
+struct ShieldView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShieldView()
+}

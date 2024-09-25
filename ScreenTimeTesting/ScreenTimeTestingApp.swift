59a1cd5299c419287c3885088f4a4467b36c1876
Iftikhar Hussain <iftikharhussain2230@gmail.com>
2024-09-26 00:05:54 +0500

    Initial Commit

---
+++ ScreenTimeTesting/ScreenTimeTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeTestingApp.swift
+//  ScreenTimeTesting
+//
+//  Created by mac on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
